0504e834d53fa355066e8ce746a3b69d2baebc74
truong hoang nguyen <truonghoangnguyen@gmail.com>
2026-09-08 14:55:57 +0700

    feat: update source directories and remove obsolete notes for "The Life of the Buddha"

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -377,7 +377,13 @@ export default defineConfig({
         name: 'copy-markdown-files',
         // This hook runs at the end of the build process
         async closeBundle() {
-          const sourceDirs = ['./docs/kinhtruongbo/', './docs/kinhtrungbo/', './docs/kinhtangchi/']; // Directories with your MD files
+          //const sourceDirs = ['./docs/kinhtruongbo/', './docs/kinhtrungbo/', './docs/kinhtangchi/']; // Directories with your MD files
+          const sourceDirs = [
+            './docs/kinhtruongbo/',
+            './docs/kinhtrungbo/',
+            './docs/kinhtangchi/',
+            './docs/the-life-of-the-buddha/',
+          ];
 
           // Fix for ESM __dirname
           const fs = await import('node:fs/promises');
